4e4f53d5ecb94a8eb043b416382a4b0e38469450
xhl <mashiro-e@qq.com>
2023-08-12 20:17:19 +0800

    线程池完成

--- main.cpp
+++ main.cpp
@@ -1,52 +1,71 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <functional>
 
-/// 单例logger, 并不严谨
-class logger;
-static logger *m_instance = nullptr;
-static std::once_flag once;
-class logger {
-private:
-  logger() = default;
-
+class thread_pool {
 public:
-
-  /// 禁用拷贝构造
-  logger(const logger &other) = delete;
-
-  /// 禁用拷贝构造(重载)
-  logger &operator=(const logger &other) = delete;
-
-  /// 饿汉单例: 提前声明, 需要时实例化
-  static logger &instance() {
-    std::call_once(once, []() {
-      if (!m_instance) m_instance = new logger();
-    });
-    return *m_instance;
+  explicit thread_pool(const int &pool_size) : m_stopped(false) {
+    this->m_threads.reserve(pool_size);
+    for (int i = 0; i < pool_size; ++i) {
+      this->m_threads.emplace_back([this]() {
+        while (true) {
+          std::unique_lock<std::mutex> u_lock(this->m_mtx);
+          m_condition.wait(u_lock, [this]() {
+            return this->m_stopped || (!this->m_tasks.empty());
+          });
+          if (m_stopped && m_tasks.empty()) return;// in lambda
+          auto task = this->m_tasks.front();
+          this->m_tasks.pop();
+          u_lock.unlock();
+          task();
+        }
+      });
+    }
   }
 
-  void info(const std::string &msg) {
-    std::cout << this << " " << __TIMESTAMP__ << " " << msg << std::endl;
+  ~thread_pool() {
+    {
+      std::unique_lock<std::mutex> lock(m_mtx);
+      m_stopped = true;
+    }
+    m_condition.notify_all();
+    for (auto &item: m_threads) {
+      item.join();
+    }
   }
+
+  template<class Func, typename... Param>
+  bool add_task(Func &&func, Param &&...param) {
+    bool succeed{false};
+    std::function<void()> task = std::bind(std::forward<Func>(func), std::forward<Param>(param)...);
+    {
+      std::unique_lock<std::mutex> u_lock(this->m_mtx);
+      succeed = (bool) this->m_tasks.emplace(std::move(task));
+    }
+    this->m_condition.notify_one();
+    return succeed;
+  }
+
+private:
+  std::vector<std::thread> m_threads;
+  std::queue<std::function<void()>> m_tasks;
+  std::mutex m_mtx;
+  std::condition_variable m_condition;
+  bool m_stopped;
 };
-
-void print_msg(const std::string &msg) {
-  logger::instance().info(msg);
-}
 
 int main() {
   /// 多线程访问get_instance可能造成instance被实例化多次
-  ///
-  std::thread t1(print_msg, "error 1\n");
-  std::thread t2(print_msg, "error 2\n");
-  std::thread t3(print_msg, "error 3\n");
-  std::thread t4(print_msg, "error 4\n");
-  std::thread t5(print_msg, "error 5\n");
-  t1.join();
-  t2.join();
-  t3.join();
-  t4.join();
-  t5.join();
+  thread_pool pool(5);
+  for (int i = 0; i < 5; ++i) {
+    std::ignore = pool.add_task([i]() {
+      std::cout << __TIMESTAMP__ << "  Task [" << i + 1 << "] starting..." << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      std::cout << __TIMESTAMP__ << "  Task [" << i + 1 << "] done" << std::endl;
+    });
+  }
   return 0;
 }
