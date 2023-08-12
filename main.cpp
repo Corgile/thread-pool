af35b9ec5fed66daa74e2a71da23e85fb2508793
xhl <mashiro-e@qq.com>
2023-08-12 16:13:28 +0800

    线程访问被释放的内存

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <thread>
 
-void foo(int &i) {
-  i += 1;
+std::thread t1;
+
+void foo(int *i) {
+  *i += 1;
+  /// VS: 输出-572662306; Clion: 程序卡死
+  std::cout << *i << std::endl;
 }
 
-std::thread t1;
-void test() {
-  int a  = 1;
-  t1 = std::thread(foo, std::ref(a));
-}
+
 int main() {
-  test();
-  /**
-   * 在Visual Studio下运行会报错：
-   * (process 17892) exited with code -1073741819
-   * 因为在test函数结束后, a已经被销毁了，此时再在main中去join()的话，
-   * t1 已经找不到&a了，因此会出错.
-   */
+  int *ptr = new int(1);
+  t1 = std::thread(foo, ptr);
+  delete ptr;
+  ptr = nullptr;
+  /// 线程启动前，某些变量被销毁，导致未定义错误
   t1.join();
   return 0;
 }
