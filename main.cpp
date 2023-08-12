c73d9710552a497d5c0e5bc9d7fa5e4dd5f9c0fc
xhl <mashiro-e@qq.com>
2023-08-12 16:04:19 +0800

    传入引用时可能出现的问题

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@ void foo(int &i) {
   i += 1;
 }
 
+std::thread t1;
+void test() {
+  int a  = 1;
+  t1 = std::thread(foo, std::ref(a));
+}
 int main() {
-  int refer = 1;
-  std::thread t1(foo, std::ref(refer));
+  test();
+  /**
+   * 在Visual Studio下运行会报错：
+   * (process 17892) exited with code -1073741819
+   * 因为在test函数结束后, a已经被销毁了，此时再在main中去join()的话，
+   * t1 已经找不到&a了，因此会出错.
+   */
   t1.join();
-  std::cout << refer << std::endl;
   return 0;
 }
