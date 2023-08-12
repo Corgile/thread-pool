92aa168ab9766c1980ab1502b33a34abcd600880
xhl <mashiro-e@qq.com>
2023-08-12 16:30:42 +0800

    访问私有成员: friend

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 #include <memory>
 
 class some_class {
-public:
+private:
+  friend void thread_foo();
+
   void foo() {
     std::cout << "hello\n";
   }
 };
 
-int main() {
+
+void thread_foo() {
   std::shared_ptr<some_class> a = std::make_shared<some_class>();
-  /// 为什么要传入a? 因为执行foo函数的时候需要实例化一个对象
-  /// 通过this->foo() 或者instance->foo()来调用foo函数,
-  /// 这个 a 相当于告诉了 t 这个this/instance是谁.
+  /// 线程访问私有成员: friend
   std::thread t(&some_class::foo, a);
   t.join();
+}
+
+int main() {
+  thread_foo();
   return 0;
 }
